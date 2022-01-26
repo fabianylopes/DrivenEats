c5abd60b3bd7f67e24a1d18c494ae69bbe41be79
Faby <fabiany.lopes.060@ufrn.edu.br>
2022-01-26 11:41:13 -0300

    finish order button

--- js/script.js
+++ js/script.js
@@ -1,38 +1,78 @@
+let cont = 0;
 
-function choosePlate(plate){
-    const border = document.querySelector('.plates.selected');
+let dish = '';
+let drink = '';
+let dessert = '';
+
+let dishPrice;
+let drinkPrice;
+let dessertPrice;
+
+
+function choosePlate(option, name, price){
+    const border = document.querySelector('.dish.selected');
     
     if(border !== null){
         border.classList.toggle('selected');
+    }else{
+        cont++;
     }
-    plate.classList.add('selected');
-    
+
+    option.classList.add('selected');
+
+    dish = name;
+    dishPrice = price;
+
+    checkOrder();
 }
 
-function chooseDrink(drink){
-    const border = document.querySelector('.drinks.selected');
-    
-    if(border !== null){
-        border.classList.toggle('selected');
-    }
-    drink.classList.add('selected');
-}
-
-function chooseDessert(dessert){
-    const border = document.querySelector('.desserts.selected');
+function chooseDrink(option, name, price){
+    const border = document.querySelector('.drink.selected');
     
     if(border !== null){
         border.classList.toggle('selected');
+    }else{
+        cont++;
     }
-    dessert.classList.add('selected');
+
+    option.classList.add('selected');
+
+    drink = name;
+    drinkPrice = price;
+
+    checkOrder();
 }
 
+function chooseDessert(option, name, price){
+    const border = document.querySelector('.dessert.selected');
+    
+    if(border !== null){
+        border.classList.toggle('selected');
+    }else{
+        cont++;
+    }
 
-function fecharPedido(){
+    option.classList.add('selected');
+
+    dessert = name;
+    dessertPrice = price;
+
+    checkOrder();
+}
+
+let orderButton = document.querySelector('button');
+orderButton.disabled = true;
+
+function checkOrder(){
+
+    if(cont === 3){
+        const text = document.querySelector('.footer-text');
+        text.innerHTML = 'Fechar pedido';
+        
+        const button = document.querySelector('button');
+        button.classList.add('button')
+        orderButton.disabled = false;
+    }
    
-    const text = document.querySelector('.footer-text');
-    text.innerHTML = 'Fechar pedido';
 }
-
-
 
